637fe50d4851293502ba442989531ed195d7ff6a
Trekki03 <33525119+Trekki03@users.noreply.github.com>
2023-01-11 17:52:44 +0100

    Added Failing Tests

--- Test/ExampleTest.cpp
+++ Test/ExampleTest.cpp
@@ -2,5 +2,21 @@
 
 TEST(TestSuite, Test1)
 {
+    EXPECT_TRUE(false);
+}
+
+TEST(TestSuite, Test2)
+{
     EXPECT_TRUE(true);
 }
+
+TEST(TestSuite, Test3)
+{
+    EXPECT_TRUE(true);
+}
+
+TEST(TestSuite, Test4)
+{
+    EXPECT_TRUE(false);
+}
+
