08b147a84615a8678d82d34517889eb2576a385e
Trekki03 <33525119+Trekki03@users.noreply.github.com>
2023-01-11 21:49:27 +0100

    Debug 10

--- Test/T3D/OpenGL/OpenGLObjectTest.cpp
+++ Test/T3D/OpenGL/OpenGLObjectTest.cpp
@@ -16,6 +16,15 @@ protected:
         if (!glfwInit())
             EXPECT_TRUE(false);
 
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+        #ifdef __APPLE__
+            glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // uncomment this statement to fix compilation on OS X
+        #endif
+
+
         /* Create a windowed mode window and its OpenGL context */
         window = glfwCreateWindow(640, 480, "OPENGL Test Fixture", NULL, NULL);
         if (!window)
