fb5a35cb953fb2c15b65584ed8484088909ba284
Trekki03 <33525119+Trekki03@users.noreply.github.com>
2023-05-13 12:09:58 +0100

    OpenGlRender Testsuite setup

---
+++ Test/T3D/openGlRenderTests/CameraTests.cpp
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+#include "T3D/openGlRender/openGlCamera/Camera.hpp"
+
+TEST(OpenGlRenderTests, cameraBindingTest)
+{
+	//Tests must be added in the future
+	EXPECT_TRUE(true);
+}
